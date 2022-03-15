3f61e69c8ec5d498cb926bcc25eb56a9e3b65338
SEONGJAE CHO <whtdjwo98@naver.com>
2022-03-15 22:34:07 +0900

    10951번 덧셈

---
+++ 10951.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+
+int main() {
+	int A, B;
+
+	while (scanf("%d %d", &A, &B) != EOF) {
+		printf("%d\n", A + B);
+	}
+
+	return 0;
+}
