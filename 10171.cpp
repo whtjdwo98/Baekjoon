eee0cf33fc56bdeb0d7332cf41273be6de33a5c8
SEONGJAE CHO <whtdjwo98@naver.com>
2022-03-15 23:44:22 +0900

    10171번 고양이

---
+++ 10171.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	printf("\\    \/\\\n");
+	printf("\\    /\\\n");
+	printf(" )  ( ')\n");
+	printf("(  /  )\n");
+	printf(" \\(__)|");
+	return 0;
+}
